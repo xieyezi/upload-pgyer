7e1f87b1a348ee94b045166144a67f2c3ed2693b
xieyezi <1435398529@qq.com>
2021-01-17 21:23:47 +0800

    feat: add some explain.

--- bin/upload-pgyer.ts
+++ bin/upload-pgyer.ts
@@ -1,10 +1,10 @@
 /**
- * @required --platform 构建的平台 android/ios
+ * @required --platform 构建的平台 (android/ios)
  * @required --filepath 上传文件的绝对路径
  * @required --webhook 要发送平台提供的webhook
  * @required --apikey 蒲公英平台提供的API key
  * @required --buildurl 本次构建的url地址
- * @required --target 发送消息的平台 支持飞书、钉钉、企业微信
+ * @required --target 发送消息的平台 支持飞书(lark)、钉钉(dingtalk)、企业微信(wechat)
  * 调用示例：
  *   node upload-pgyer.js --filepath='xxxx' --apikey='xxxx' --webhook='xxxx' --buildurl='https://www.baidu.com/' --platform='android' --target='lark'
  */
